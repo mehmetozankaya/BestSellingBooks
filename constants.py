97276527919e67b48202aa826db42e7c0861afa2
mehmetozankaya <71948708+mehmetozankaya@users.noreply.github.com>
2021-09-03 14:25:56 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,3 @@
-DB_CONN_STRING = 'mongodb+srv://m001-student:MongoPass123@cluster0.dnaq2.mongodb.net/forbes?retryWrites=true&w=majority'
-DB_NAME = 'forbes'
-TABLE_NAME= 'forbes_data'
-
-# DB_CONN_STRING = 'mongodb+srv://ozan:kaya@cluster0.2v7o6.mongodb.net/<dbname>?retryWrites=true&w=majority'
-# DB_NAME = 'AmazonData'
-# TABLE_NAME= 'BestBooks'
+DB_CONN_STRING = ''
+DB_NAME = ''
+TABLE_NAME= ''
